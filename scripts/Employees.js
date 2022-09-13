6e22dd49d8e82c06c4e22f6ccf21c84d134a320c
dhollifield <deannahollifield@gmail.com>
2022-09-13 16:58:10 -0500

    initial commit

---
+++ scripts/Employees.js
@@ -0,0 +1,18 @@
+import { getEmployees, getOrders } from "./database.js"
+
+const employees = getEmployees()
+
+const orders = getOrders()
+
+export const Employees = () => {
+    let html = "<ul>"
+
+    for (const employee of employees) {
+        html += `<li id="employee--${employee.id}">${employee.name}</li>`
+    }
+
+    html += "</ul>"
+
+    return html
+}
+
